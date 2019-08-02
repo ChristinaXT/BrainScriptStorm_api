95aeb7b3374160c090338d77f587b7496a20e527
Guess <guess@Guesss-iMac.local>
2019-08-02 11:36:48 -0400

    setting up User

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+# I want my api to be BrainScriptStorm.com/api/v1/resource
+namespace :api do
+  namespace :v1 do
+  resources :users
+ end
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
