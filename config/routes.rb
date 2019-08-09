353e0f6593cdafbd8a7240bac9c62aec812ff85f
Guess <guess@Guesss-iMac.local>
2019-08-09 15:38:01 -0400

    added to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  post "/api/v1/signup", to: "api/v1/users#create"
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
 # I want my api to be BrainScriptStorm.com/api/v1/resource
-namespace :api do
-  namespace :v1 do
-  resources :inciting_incidents
-  resources :epoches
-  resources :given_circumstances
-  resources :characters
-  resources :titles
-  resources :stories
-  resources :locations
-  resources :users
- end
-end
+  namespace :api do
+    namespace :v1 do
+      resources :inciting_incidents
+      resources :epoches
+      resources :given_circumstances
+      resources :characters
+      resources :titles
+      resources :stories
+      resources :locations
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
